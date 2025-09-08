19bf4ce8661e2b1a96f0194aa951f586c5eeec7a
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-08 18:09:10 +0000

    adding review1

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,4 +1,3 @@
-
 class Notes {
 	public static void main(String[] args) {
 
--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,34 @@
 public class Review1 {
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int number1;
+		int number2;
+		number1 = 9;
+		number2 = 5;
+		System.out.println(number1/number2);
+		System.out.println(number1%number2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		System.out.println(true);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String word1;
+		String word2;
+		word1 = "Hello";
+		word2 = "World";
+		System.out.print(word1);
+		System.out.print(" ");
+		System.out.print(word2);
 	}
 
 }
