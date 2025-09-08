4b2a2010825665ab6f17bf93dfc19a9eebbd36e5
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-08 18:49:45 +0000

    the

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,5 +1,11 @@
 public class Review1 {
-
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
 	public static void q1() {
 		System.out.println("There once was a man from St. Ives.");
 	}
